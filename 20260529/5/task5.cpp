328d11a060e0702f1f4bb0bd03c3e1f571c48eec
yuto <humanyukusiku@gmail.com>
2026-06-12 17:26:53 +0900

    0529task5
    インデント等の修正

--- 20260529/5/task5.cpp
+++ 20260529/5/task5.cpp
@@ -1,9 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     for(int answer = 1; answer <= 10; answer++)
-    if(answer % 3 != 0)
-    cout << answer << endl;
+    {
+        if(answer % 3 != 0)
+        {
+             cout << answer << endl;
+        }
+    }
+    return 0;
 }
