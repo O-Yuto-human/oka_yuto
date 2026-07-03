d47feac6fed318b616632e4f9bd73be811c9193f
yuto <humanyukusiku@gmail.com>
2026-07-03 17:50:17 +0900

    0619task1
    バブルソート前、後の乱数表示

--- 20260619/1/task1.cpp
+++ 20260619/1/task1.cpp
@@ -10,18 +10,28 @@ int main()
 
     int array[Define::SIZE];
 
-    for(int i = 0; i < Define::SIZE; i++)
+    for(int i = 0; i < Define::SIZE; i++) // 100個の乱数作成
     {
         array[i] = rand() % (Define::MAX - Define::MIN) + Define::MIN;
         cout << array[i] << "\n";
+    }
 
-        if(array[i] > array[i + Define::current])
+    for(int j = 0; j < Define::SIZE; j++) // バブルソート
+    {
+        for(int k = 0; k < Define::SIZE; k++)
         {
-            int box = array[i];
-            array[i] = array[i + Define::current];
-            array[i + Define::current] = box;
+            if(array[k] > array[k + Define::current])
+                {
+                    int box = array[k];
+                    array[k] = array[k + Define::current];
+                    array[k + Define::current] = box;
+                }
         }
     }
-    cout << endl;
+
+    for(int l = 0; l < Define::SIZE; l++) // 表示
+    {
+        cout << "\n" << array[l];
+    }
     return 0;
 }
